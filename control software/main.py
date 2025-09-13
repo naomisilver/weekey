9b4f9b94fef843db25544ead51b288d6f82a6e26
naomisilver <2425281@dundee.ac.uk>
2025-09-14 00:03:07 +0100

    V1.1 Added the ability for background app using pyinstaller
    Added (crudely drawn) icon image, updated main.py to support pystray for background process and hidden app and spec file for pyinstaller

--- control software/main.py
+++ control software/main.py
@@ -1,23 +1,32 @@
 """
     - WeeKey control main file
     - Naomi Silver September 2025
-    - V1.0
+    - V1.1 - added background process support
 """
-
+from win32con import VK_MEDIA_PLAY_PAUSE, VK_MEDIA_NEXT_TRACK, VK_MEDIA_PREV_TRACK, KEYEVENTF_EXTENDEDKEY, KEYEVENTF_KEYUP, VK_VOLUME_DOWN, VK_VOLUME_MUTE, VK_VOLUME_UP
 import yaml
 import os
+import sys
 import keyboard
 import time
 import subprocess
 import threading
 import win32api
-from win32con import VK_MEDIA_PLAY_PAUSE, VK_MEDIA_NEXT_TRACK, VK_MEDIA_PREV_TRACK, KEYEVENTF_EXTENDEDKEY, KEYEVENTF_KEYUP, VK_VOLUME_DOWN, VK_VOLUME_MUTE, VK_VOLUME_UP
+import pystray
+from PIL import Image
 
-SCRIPT_FILE = os.path.dirname(os.path.abspath(__file__))
-CONFIG_FILE = os.path.join(SCRIPT_FILE, "config.yml") 
+OS = sys.platform # to be used in the future when *maybe* working on cross-platform support
+
+def resource_path(filename):
+    #if getattr(sys, 'frozen', False):
+        #base_path = getattr(sys, "_MEIPASS", os.path.dirname(sys.executable)) # UNECESSARY BECAUSE OF --onedir... I WANT TO USE --onefile but can't find out how :((((
+    #else: # pyinstaller nonsense, need to set different paths depending if it is frozen (bundled exe) or running in an editor/shell
+    base_path = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(base_path, filename)
 
 def load_config():
-    with open(CONFIG_FILE, 'r') as f:
+    config_path = resource_path("config.yml")
+    with open(config_path, 'r') as f:
         config = yaml.safe_load(f)
     return config.get("keys", {})
 
@@ -43,16 +52,16 @@ def handle_action(action): # action handling
             media = action[6:].strip()
             if media == ("play_pause"):
                 win32api.keybd_event(VK_MEDIA_PLAY_PAUSE, 0, 0); win32api.keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_KEYUP, 0) # press and release using win32api
-                time.sleep(0.2) # additional debounce, stops it from rapidly pausing and unpausing
+                time.sleep(0.3) # additional debounce, stops it from rapidly pausing and unpausing
             if media == ("next"):
                 win32api.keybd_event(VK_MEDIA_NEXT_TRACK, 0, 0); win32api.keybd_event(VK_MEDIA_NEXT_TRACK, 0, KEYEVENTF_KEYUP, 0)
-                time.sleep(0.2)
+                time.sleep(0.3)
             if media == ("previous"):
                 win32api.keybd_event(VK_MEDIA_PREV_TRACK, 0, 0); win32api.keybd_event(VK_MEDIA_PREV_TRACK, 0, KEYEVENTF_KEYUP, 0)
-                time.sleep(0.2)
+                time.sleep(0.3)
             if media == ("vol_mute"):
                 win32api.keybd_event(VK_VOLUME_MUTE, 0, KEYEVENTF_EXTENDEDKEY); win32api.keybd_event(VK_VOLUME_MUTE, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP)
-                time.sleep(0.2)
+                time.sleep(0.3)
 
         elif action.startswith("shortcut:"):
             shortcut = action[9:].strip()
@@ -77,13 +86,34 @@ def key_updater(): # update the config and key binds to refresh every 5 seconds
         keyboard.unhook_all_hotkeys() # remove old hotkeys
         # print("hotkeys reloaded")
 
+def on_quit(icon, item):
+    icon.stop() # replaces keyboard.wait()
+
+def open_config(icon, item):
+    config_path = resource_path("config.yml") 
+    if OS == "win32": # maybe working on multi-platform support?
+        os.startfile(config_path)
+    else:
+        print("multiple OS' not working yet :D") 
 
 def main():
     thread = threading.Thread(target=key_updater, daemon=True) # new thread 
     thread.start()
 
-    keyboard.wait()
+    icon_path = resource_path("icon.png") # crudely drawn icon :D
+    icon_image = Image.open(icon_path)
 
+    icon = pystray.Icon(
+        "WeeKey",
+        icon = icon_image,
+        title = "WeeKey Control",
+        menu = pystray.Menu(
+            pystray.MenuItem("Open Config", open_config), # on right click show "open config" option, allows for quickly editing the config
+            pystray.MenuItem("Quit", on_quit), # same thing but closes the program by stopping the icon and quiting the program
+        )
+    )
+
+    icon.run()
 
 if __name__ == "__main__":
     main()
