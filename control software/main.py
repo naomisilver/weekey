4ae4727b381fc0c6fa1d93ef4adf05d3632153a6
naomisilver <2425281@dundee.ac.uk>
2025-09-14 23:09:16 +0100

    V1.2 Updated main.py
    Added notification support through pystray

--- control software/main.py
+++ control software/main.py
@@ -1,8 +1,13 @@
 """
     - WeeKey control main file
     - Naomi Silver September 2025
-    - V1.1 - added background process support
+    - V1.2 - added notification support through pystray
 """
+
+"""
+    TODO:
+"""
+
 from win32con import VK_MEDIA_PLAY_PAUSE, VK_MEDIA_NEXT_TRACK, VK_MEDIA_PREV_TRACK, KEYEVENTF_EXTENDEDKEY, KEYEVENTF_KEYUP, VK_VOLUME_DOWN, VK_VOLUME_MUTE, VK_VOLUME_UP
 import yaml
 import os
@@ -15,12 +20,11 @@ import win32api
 import pystray
 from PIL import Image
 
+notifs_show = False # gives the user the option to turn notifs on/off
+icon = None # used for notifications
 OS = sys.platform # to be used in the future when *maybe* working on cross-platform support
 
 def resource_path(filename):
-    #if getattr(sys, 'frozen', False):
-        #base_path = getattr(sys, "_MEIPASS", os.path.dirname(sys.executable)) # UNECESSARY BECAUSE OF --onedir... I WANT TO USE --onefile but can't find out how :((((
-    #else: # pyinstaller nonsense, need to set different paths depending if it is frozen (bundled exe) or running in an editor/shell
     base_path = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(base_path, filename)
 
@@ -34,49 +38,64 @@ def bind_keys(key_map):
     try:
         for key, action in key_map.items(): # for each key and action in key_map
             keyboard.add_hotkey(key, lambda a=action: handle_action(a))
-    except AttributeError: # dont error if there isnt an action 
-        print("keybind needs action")
+    except AttributeError as e: # dont error if there isnt an action 
+        notify(f"Error key'{key} not assigned': {e}", icon)
 
 def handle_action(action): # action handling
+
     try:
 
         if action.startswith("run:"): 
             cmd = action[4:].strip() # strip the command part of the config setup
             subprocess.Popen(cmd, shell=False) # run shell command to open
+            notify(f"running command: {cmd}", icon)
+            time.sleep(0.1)
 
         elif action.startswith("type:"):
             text = action[5:].strip()
             keyboard.write(text)
+            notify(f"writing text: {text}", icon)
+            time.sleep(0.1)
 
         elif action.startswith("media:"):
             media = action[6:].strip()
             if media == ("play_pause"):
                 win32api.keybd_event(VK_MEDIA_PLAY_PAUSE, 0, 0); win32api.keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_KEYUP, 0) # press and release using win32api
+                notify(f"running: {media}", icon)
                 time.sleep(0.3) # additional debounce, stops it from rapidly pausing and unpausing
             if media == ("next"):
                 win32api.keybd_event(VK_MEDIA_NEXT_TRACK, 0, 0); win32api.keybd_event(VK_MEDIA_NEXT_TRACK, 0, KEYEVENTF_KEYUP, 0)
+                notify(f"running: {media}", icon)
                 time.sleep(0.3)
             if media == ("previous"):
                 win32api.keybd_event(VK_MEDIA_PREV_TRACK, 0, 0); win32api.keybd_event(VK_MEDIA_PREV_TRACK, 0, KEYEVENTF_KEYUP, 0)
+                notify(f"running: {media}", icon)
                 time.sleep(0.3)
             if media == ("vol_mute"):
                 win32api.keybd_event(VK_VOLUME_MUTE, 0, KEYEVENTF_EXTENDEDKEY); win32api.keybd_event(VK_VOLUME_MUTE, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP)
+                notify(f"running: {media}", icon)
                 time.sleep(0.3)
 
         elif action.startswith("shortcut:"):
             shortcut = action[9:].strip()
             keyboard.press_and_release(shortcut)
+            notify(f"running: {shortcut}", icon)
             time.sleep(0.2)
 
         elif action.startswith("browser:"):
             url = action[8:].strip()
             os.startfile(url)
+            notify(f"opening: {url}", icon)
+            time.sleep(0.2)
 
         else:
             print("not valid macro")
 
-    except AttributeError: # doesnt crash if the keybind doesnt have action, crashes if something else is wrong
-        print("keybind needs action")
+    except AttributeError as e: # doesnt crash if the keybind doesnt have action, crashes if something else is wrong
+        notify(f"Keybind needs action | Error: {e}", icon)
+    
+    except Exception as e:
+        notify(f"Unexpected error: {e}", icon) 
 
 def key_updater(): # update the config and key binds to refresh every 5 seconds
     while True:
@@ -84,7 +103,6 @@ def key_updater(): # update the config and key binds to refresh every 5 seconds
         bind_keys(key_map)
         time.sleep(5)
         keyboard.unhook_all_hotkeys() # remove old hotkeys
-        # print("hotkeys reloaded")
 
 def on_quit(icon, item):
     icon.stop() # replaces keyboard.wait()
@@ -96,7 +114,24 @@ def open_config(icon, item):
     else:
         print("multiple OS' not working yet :D") 
 
+def notify(msg, icon=None):
+
+    print(msg)
+
+    if icon and notifs_show:
+        try:
+            icon.notify(msg)
+        except Exception as e:
+            print(f"Notification failed: {e}") # if the notification warning you of an error, errors at least print it :D
+
+
+def toggle_notifs(icon, item):
+    global notifs_show
+    notifs_show = not notifs_show
+
 def main():
+    global icon
+
     thread = threading.Thread(target=key_updater, daemon=True) # new thread 
     thread.start()
 
@@ -108,6 +143,7 @@ def main():
         icon = icon_image,
         title = "WeeKey Control",
         menu = pystray.Menu(
+            pystray.MenuItem("Show Notifications", toggle_notifs, checked=lambda item: notifs_show),
             pystray.MenuItem("Open Config", open_config), # on right click show "open config" option, allows for quickly editing the config
             pystray.MenuItem("Quit", on_quit), # same thing but closes the program by stopping the icon and quiting the program
         )
